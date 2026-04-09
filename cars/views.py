43f8cae451e208b5379eae33cc094ca42aa90d3d
Erick_Mazur <mazur.negocios@gmail.com>
2026-04-09 07:29:13 -0300

    upload

---
+++ cars/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from cars.models import Car
+# Create your views here.
+
+def car_view(request):
+    cars = Car.objects.all().order_by('model_year')
+    search = request.GET.get('search')
+    if search:
+        cars = Car.objects.filter(model__icontains = search)
+
+    return render(
+        request, 
+        'cars.html',
+        {'cars' : cars}
+    )
+
+def index(request):
+    return render(request, 'index.html')
