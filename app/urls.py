01d639c97f9db76169f68854beceaac1328a1d04
Erick_Mazur <mazur.negocios@gmail.com>
2026-04-14 10:46:03 -0300

    register + login
    sistema de registro + login com os formularios do django

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import car_view, index, new_car_view
+from accounts.views import register_view, login_view, logout_view
 
 
 
@@ -27,5 +28,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('cars/', car_view, name='cars_list'),
-    path('new_car/', new_car_view, name='new_car')
+    path('new_car/', new_car_view, name='new_car'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
