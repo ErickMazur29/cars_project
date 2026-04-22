2f4455c7c7fba4d9da9d4d4328248670dc86a50a
Erick_Mazur <mazur.negocios@gmail.com>
2026-04-22 07:53:56 -0300

    adição de create view no projeto
    alteração na NewCarView para uma CreateView, ampliando as boas praticas e clareza no codigo + mudança no index

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import index, CarsListView, NewCarView
+from cars.views import index, CarsListView, NewCarCreateView
 from accounts.views import register_view, login_view, logout_view
 
 
@@ -28,7 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('cars/', CarsListView.as_view(), name='cars_list'),
-    path('new_car/', NewCarView.as_view(), name='new_car'),
+    path('new_car/', NewCarCreateView.as_view(), name='new_car'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 def index(request):
     return render(request, 'index.html')
@@ -22,16 +21,9 @@ class CarsListView(ListView):
         return cars
 
 
-class NewCarView(View):
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form })
-    
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form': new_car_form })
-
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
         
