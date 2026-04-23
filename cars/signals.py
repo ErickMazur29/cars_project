d2690039b5ed4a8d1089ce8b0d3ac44b90b04dfd
ErickMazur29 <mazur.negocios@gmail.com>
2026-04-23 19:58:46 -0300

    Implementação de funções post no signals

--- cars/signals.py
+++ cars/signals.py
@@ -1,21 +1,30 @@
-from django.db.models.signals import pre_save, post_save, pre_delete, post_delete
+from django.db.models.signals import  post_save, post_delete
+from django.db.models import Sum
 from django.dispatch import receiver
-from cars.models import Car
+from cars.models import Car, CarInventory
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print('### PRE SAVE REALIZED ###')
+
+#Função para autalização de inventario de carros
+def car_inventory_update():
+    cars_count = Car.objects.count() # pega o valor da qtd de carros
+    cars_value = Car.objects.aggregate( # pega o valor de todos os carros (preço)
+        total_value = Sum('value') # faz a soma
+    )['total_value'] # mostra apenas o valor total, não o dicionario
+
+    CarInventory.objects.create( # cria/alimenta o bando de dados do inventario com os novos valores
+        cars_count = cars_count,
+        cars_value = cars_value
+    )
+
+
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print('### POST SAVE REALIZED ###')
+    car_inventory_update()
 
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    print('### PRE DELETE REALIZED ###')
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    print('### POST DELETE REALIZED ###')
+    car_inventory_update()
 
 
